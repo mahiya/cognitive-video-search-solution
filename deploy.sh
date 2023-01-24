d421888bffef72042045d18ce67a762f31aac68b
Masayuki Hiyama <mahiyama@microsoft.com>
2023-01-24 13:58:39 +0900

    ビデオ分析のワークフロー処理を Logic Apps で行うように修正

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,10 @@
 # 変数を定義する
 region='japaneast'                       # デプロイ先のリージョン
 resourceGroupName=$1                     # デプロイ先のリソースグループ (スクリプトの引数から取得する)
-blobContainerName='uploaddata'           # ビデオファイルアップロードに使用する Blob コンテナ名
+uploadedBlobContainerName='uploaddata'   # ビデオファイルアップロードに使用する Blob コンテナ名
+analyzedBlobContainerName='analyzed'     # Video Indexer での分析結果の JSON ファイルを格納する Blob コンテナ名
 cognitiveSearchIndexName='video-phrases' # Cognitive Search のインデックス名
-functionName='ProcessUploadedData'       # Azure Functions アプリケーションコードで指定したものと同じにする
+logicAppTriggerName='webhook'            # Logic Apps のワークフロー定義でのトリガーの名前
 
 # リソースグループを作成する
 az group create \
@@ -16,45 +17,101 @@ az group create \
 outputs=($(az deployment group create \
             --resource-group $resourceGroupName \
             --template-file biceps/deploy.bicep \
-            --parameters blobContainerName=$blobContainerName \
-                         cognitiveSearchIndexName=$cognitiveSearchIndexName \
+            --parameters uploadedBlobContainerName=$uploadedBlobContainerName \
+                         analyzedBlobContainerName=$analyzedBlobContainerName \
             --query 'properties.outputs.*.value' \
             --output tsv))
 tenantId=`echo ${outputs[0]}` # 文末の \r を削除する
-storageAccountName=`echo ${outputs[1]}` # 文末の \r を削除する
-functionAppForWorkflowName=`echo ${outputs[2]}` # 文末の \r を削除する
-functionAppForWebApiName=`echo ${outputs[3]}` # 文末の \r を削除する
+subscriptionId=`echo ${outputs[1]}` # 文末の \r を削除する
+storageAccountName=`echo ${outputs[2]}` # 文末の \r を削除する
+functionAppName=`echo ${outputs[3]}` # 文末の \r を削除する
 staticWebAppName=`echo ${outputs[4]}` # 文末の \r を削除する
 staticWebAppHostName=`echo ${outputs[5]}` # 文末の \r を削除する
 cognitiveSearchName=`echo ${outputs[6]}` # 文末の \r を削除する
-videoIndexerAccountId=`echo ${outputs[7]}` # 文末の \r を削除する
-videoIndexerLocation=${outputs[8]}
+cognitiveServiceName=`echo ${outputs[7]}` # 文末の \r を削除する
+videoIndexerResourceId=`echo ${outputs[8]}` # 文末の \r を削除する
+videoIndexerAccountId=`echo ${outputs[9]}` # 文末の \r を削除する
+videoIndexerLocation=`echo ${outputs[10]}` # 文末の \r を削除する
+logicAppName=`echo ${outputs[11]}` # 文末の \r を削除する
+logicAppConnectionId=${outputs[12]}
+
+# Cognitive Service の API キーを取得する
+cognitiveServiceKey=`az cognitiveservices account keys list --name $cognitiveServiceName --resource-group $resourceGroupName --query 'key1' --output tsv`
+
+# Cognitive Search の API キーを取得する
+cognitiveSearchApiKey=`az search admin-key show --service-name $cognitiveSearchName --resource-group $resourceGroupName --query 'primaryKey' --output tsv`
+
+# Cognitive Search インデックスを作成する
+cognitiveSearchIndexName="video-phrases"
+curl -X PUT https://$cognitiveSearchName.search.windows.net/indexes/$cognitiveSearchIndexName?api-version=2020-06-30 \
+    -H 'Content-Type: application/json' \
+    -H 'api-key: '$cognitiveSearchApiKey \
+    -d @cogsearch/index.json
+
+# Cognitive Search データソースを作成する
+cognitiveSearchDataSourceName="video-phrases"
+curl -X PUT https://$cognitiveSearchName.search.windows.net/datasources/$cognitiveSearchDataSourceName?api-version=2020-06-30 \
+    -H 'Content-Type: application/json' \
+    -H 'api-key: '$cognitiveSearchApiKey \
+    -d "$(sed -e "s|{{CONNECTION_STRING}}|ResourceId=/subscriptions/$subscriptionId/resourceGroups/$resourceGroupName/providers/Microsoft.Storage/storageAccounts/$storageAccountName;|; \
+                  s|{{CONTAINER_NAME}}|$analyzedBlobContainerName|;" \
+                  "cogsearch/datasource.json")"
+
+# Cognitive Search スキルセットを作成する
+cognitiveSearchSkillSetName="video-phrases"
+curl -X PUT https://$cognitiveSearchName.search.windows.net/skillsets/$cognitiveSearchSkillSetName?api-version=2020-06-30 \
+    -H 'Content-Type: application/json' \
+    -H 'api-key: '$cognitiveSearchApiKey \
+    -d "$(sed -e "s|{{COGNITIVE_SERVICE_KEY}}|$cognitiveServiceKey|;" \
+                  "cogsearch/skillset.json")"
+
+
+# Cognitive Search インデクサーを作成する
+cognitiveSearchIndexerName="video-phrases"
+curl -X PUT https://$cognitiveSearchName.search.windows.net/indexers/$cognitiveSearchIndexerName?api-version=2020-06-30 \
+    -H 'Content-Type: application/json' \
+    -H 'api-key: '$cognitiveSearchApiKey \
+    -d "$(sed -e "s|{{DATASOURCE_NAME}}|$cognitiveSearchDataSourceName|; \
+                  s|{{SKILLSET_NAME}}|$cognitiveSearchSkillSetName|; \
+                  s|{{INDEX_NAME}}|$cognitiveSearchIndexName|;" \
+                  "cogsearch/indexer.json")"
 
 # Azure Functions のアプリケーションをデプロイする
-pushd functions/Workflow
+pushd functions
 sleep 10 # Azure Functions App リソースの作成からコードデプロイが早すぎると「リソースが見つからない」エラーが発生する場合があるので、一時停止する
-func azure functionapp publish $functionAppForWorkflowName --csharp
+func azure functionapp publish $functionAppName --csharp
 popd
 
-pushd functions/WebApi
-func azure functionapp publish $functionAppForWebApiName --csharp
-popd
+# Azure Logic Apps のワークフローの定義をテンプレートから作成する
+sed -e " \
+    s|{{VIDEO_INDEXER_RESOURCE_ID}}|$videoIndexerResourceId|; \
+    s|{{VIDEO_INDEXER_ACCOUNT_ID}}|$videoIndexerAccountId|; \
+    s|{{VIDEO_INDEXER_LOCATION}}|$videoIndexerLocation|; \
+    s|{{COGNITIVE_SEARCH_NAME}}|$cognitiveSearchName|; \
+    s|{{COGNITIVE_API_KEY}}|$cognitiveSearchApiKey|; \
+    s|{{COGNITIVE_SEARCH_INDEXER_NAME}}|$cognitiveSearchIndexerName|; \
+    s|{{COGNITIVE_SEARCH_DATASOURCE_CONTAINER}}|$analyzedBlobContainerName|; \
+    s|{{LOGIC_APP_TRIGGER_NAME}}|$logicAppTriggerName|; \
+    s|{{LOGIC_APP_CONNECTION_RESOURCE_ID}}|$logicAppConnectionId|; \
+    s|{{SUBSCRIPTION_ID}}|$subscriptionId|; \
+    s|{{REGION}}|$region|; \
+" "logicapp/definition_template.json" > "logicapp/definition.json"
+
+# Azure Logic Apps のワークフローの定義をする
+az logic workflow create \
+    --location $region \
+    --resource-group $resourceGroupName \
+    --name $logicAppName \
+    --definition "logicapp/definition.json"
 
 # Azure リソース(EventGrid)をデプロイする
 az deployment group create \
     --resource-group $resourceGroupName \
     --template-file biceps/post-deploy.bicep \
     --parameters storageAccountName=$storageAccountName \
-                 blobContainerName=$blobContainerName \
-                 functionAppName=$functionAppForWorkflowName \
-                 functionName=$functionName
-
-# Cognitive Search のインデックスを作成する
-cognitiveSearchApiKey=`az search admin-key show --service-name $cognitiveSearchName --resource-group $resourceGroupName --query 'primaryKey' --output tsv`
-curl -X PUT https://$cognitiveSearchName.search.windows.net/indexes/$cognitiveSearchIndexName?api-version=2020-06-30 \
-    -H 'Content-Type: application/json' \
-    -H 'api-key: '$cognitiveSearchApiKey \
-    -d @cogsearch/index.json
+                 blobContainerName=$uploadedBlobContainerName \
+                 logicAppName=$logicAppName \
+                 logicAppTriggerName=$logicAppTriggerName
 
 # Cognitive Search へアクセスするためのクエリキーを取得する
 cognitiveSearchQueryKey=`az search query-key list --resource-group $resourceGroupName --service-name $cognitiveSearchName --query "[0].key" --output tsv`
@@ -64,6 +121,7 @@ echo "{
     \"cognitiveSearchName\": \"$cognitiveSearchName\", 
     \"cognitiveSearchIndexName\": \"$cognitiveSearchIndexName\", 
     \"cognitiveSearchQueryKey\": \"$cognitiveSearchQueryKey\",
+    \"suggesterName\": \"sg\",
     \"facetNames\": {},
     \"apiVersion\": \"2020-06-30\",
     \"searchTop\": 5,
